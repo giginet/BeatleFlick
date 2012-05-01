a6602e44c61812412160e8a18059c3c05bba9645
giginet <giginet.net@gmail.com>
2012-05-01 09:37:10 +0900

    Initial Commit

---
+++ Projectfiles/Scene/MainLayer.h
@@ -0,0 +1,13 @@
+//
+//  MainLayer.h
+//  FlickFreak
+//
+//  Created by  on 2012/5/1.
+//  Copyright (c) 2012 Kawaz. All rights reserved.
+//
+
+#import "CCLayer.h"
+
+@interface MainLayer : CCLayer
+
+@end
---
+++ Projectfiles/Scene/MainLayer.m
@@ -0,0 +1,13 @@
+//
+//  MainLayer.m
+//  FlickFreak
+//
+//  Created by  on 2012/5/1.
+//  Copyright (c) 2012 Kawaz. All rights reserved.
+//
+
+#import "MainLayer.h"
+
+@implementation MainLayer
+
+@end
